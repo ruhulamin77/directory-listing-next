857e35ac8d6ecbfa2a641646bc8ca50abcec7977
ImtiazMahmod <imtiazmahmod2@gmail.com>
2022-06-20 15:38:45 +0600

    admin dashboard

--- components/Dashboard/Sidebar/Sidebar.js
+++ components/Dashboard/Sidebar/Sidebar.js
@@ -52,43 +52,43 @@ const Sidebar = () => {
                 title="Dashboard"
                 icon={<FontAwesomeIcon icon={faChartLine} />}
               >
-                <Link href="/dashboard/home">
+                <Link href="/admin/dashboard">
                   <a>Dashboard</a>
                 </Link>
               </MenuItem>
 
               <MenuItem icon={<FontAwesomeIcon icon={faHeader} />}>
-                <Link href="/dashboard/headers">
+                <Link href="/admin/dashboard/headers">
                   <a>Headers</a>
                 </Link>
               </MenuItem>
 
               <MenuItem icon={<FontAwesomeIcon icon={faAd} />}>
-                <Link href="/dashboard/new-post-ads">
+                <Link href="/admin/dashboard/new-post-ads">
                   <a> Post Ads</a>
                 </Link>
               </MenuItem>
               <MenuItem icon={<FontAwesomeIcon icon={faSearch} />}>
-                <Link href="/dashboard/search-ads">
+                <Link href="/admin/dashboard/search-ads">
                   <a>Search Ads</a>
                 </Link>
               </MenuItem>
               <MenuItem icon={<CgProfile />}>
-                <Link href="/dashboard/profile">
+                <Link href="/admin/dashboard/profile">
                   <a>Profile</a>
                 </Link>
               </MenuItem>
               <MenuItem icon={<FontAwesomeIcon icon={faUsers} />}>
-                <Link href="/dashboard/users">
+                <Link href="/admin/dashboard/users">
                   <a>Users</a>
                 </Link>
               </MenuItem>
               <MenuItem icon={<FontAwesomeIcon icon={faPager} />}>
-                <Link href="/dashboard/pages">
+                <Link href="/admin/dashboard/pages">
                   asfas
                   {/* <SubMenu title=" Pages">
                     <MenuItem>
-                      <Link href="/dashboard/addNewPage">
+                      <Link href="/admin/dashboard/addNewPage">
                         <a>Add New Page</a>
                       </Link>
                     </MenuItem>
@@ -97,31 +97,31 @@ const Sidebar = () => {
               </MenuItem>
 
               <MenuItem icon={<FontAwesomeIcon icon={faAd} />}>
-                <Link href="/dashboard/categories">
+                <Link href="/admin/dashboard/categories">
                   <a>Categories</a>
                 </Link>
               </MenuItem>
               <MenuItem icon={<GoLocation />}>
-                <Link href="/dashboard/manage-locations">
+                <Link href="/admin/dashboard/manage-locations">
                   <a>Manage Locations</a>
                 </Link>
               </MenuItem>
               <MenuItem icon={<FontAwesomeIcon icon={faDollar} />}>
-                <Link href="/dashboard/payment">
+                <Link href="/admin/dashboard/payment">
                   <a>Payment</a>
                 </Link>
               </MenuItem>
               <MenuItem
                 icon={<GrPlan style={{ backgroundColor: "lightgray" }} />}
               >
-                <Link href="/dashboard/packages">
+                <Link href="/admin/dashboard/packages">
                   <a>Packages</a>
                 </Link>
               </MenuItem>
               <MenuItem
                 icon={<MdSettings style={{ backgroundColor: "lightgray" }} />}
               >
-                <Link href="/dashboard/settings">
+                <Link href="/admin/dashboard/settings">
                   <a>Settings</a>
                 </Link>
               </MenuItem>
@@ -130,7 +130,7 @@ const Sidebar = () => {
           <SidebarFooter className=" text-center">
             {/* <Link
               className="text-decoration-none text-secondary"
-              href="/dashboard/footers"
+              href="/admin/dashboard/footers"
             >
               {" "}
               <a>
@@ -138,33 +138,33 @@ const Sidebar = () => {
                 <Menu iconShape="square">
                   <SubMenu title=" Footers">
                     <MenuItem>
-                      <Link href="/dashboard/detail">
+                      <Link href="/admin/dashboard/detail">
                         <a>Detail</a>
                       </Link>
                     </MenuItem>
                     <SubMenu title="Columns">
                       <MenuItem>
-                        <Link href={`/dashboard/columns/col${1}`}>Col 1</Link>{" "}
+                        <Link href={`/admin/dashboard/columns/col${1}`}>Col 1</Link>{" "}
                       </MenuItem>
                       <MenuItem>
                         {" "}
-                        <Link href={`/dashboard/columns/col${2}`}>
+                        <Link href={`/admin/dashboard/columns/col${2}`}>
                           Col 2
                         </Link>{" "}
                       </MenuItem>
                       <MenuItem>
                         {" "}
-                        <Link href={`/dashboard/columns/col${3}`}>
+                        <Link href={`/admin/dashboard/columns/col${3}`}>
                           Col 3
                         </Link>{" "}
                       </MenuItem>
                       <MenuItem>
                         {" "}
-                        <Link href={`/dashboard/columns/col${4}`}>Col 4</Link>
+                        <Link href={`/admin/dashboard/columns/col${4}`}>Col 4</Link>
                       </MenuItem>
                     </SubMenu>
                     <MenuItem>
-                      <Link href="/dashboard/subFooter">Sub Footer </Link>
+                      <Link href="/admin/dashboard/subFooter">Sub Footer </Link>
                     </MenuItem>
                   </SubMenu>
                 </Menu>
