3326629000725c7eed9eee62e8b64133f7b95cc2
ImtiazMahmod <imtiazmahmod2@gmail.com>
2022-06-22 10:46:17 +0600

    authentication

---
+++ pages/admin/dashboard/headers.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { DashboardLayout, Headers } from "../../../components";
+
+export default function HeadersPage() {
+  return (
+    <>
+      <div>
+        <DashboardLayout>
+          <Headers />
+        </DashboardLayout>
+      </div>{" "}
+    </>
+  );
+}
